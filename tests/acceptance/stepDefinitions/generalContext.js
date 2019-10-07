4f93e6bd3419ff95e2c07d7dc0a817cbd6c184d6
mhe <mail@marcus-herrmann.com>
2019-10-07 10:41:58 +0200

    Add one more approach

--- tests/acceptance/stepDefinitions/generalContext.js
+++ tests/acceptance/stepDefinitions/generalContext.js
@@ -68,11 +68,13 @@ After(function () {
   createdFiles.forEach(fileName => fs.unlinkSync(fileName))
 })
 
-Then('they will find only one main navigation element', function () {
+Then('they will find only one main element', function () {
   // What I want to achieve: return client.expect.elements('main').count.to.equal(1)
   // Does not work: console.log(client.page.phoenixPage().waitForElementVisible('@mainEl'))
   // The following returns: "no such element: Unable to locate element:"
-  return client.element('css selector', 'main', function (result) {
-    console.log(result)
-  })
+  // return client.element('css selector', 'main', function (result) {
+  //   console.log(result)
+  // })
+  // The following line yields: Expected elements <Element [name=@mainEl]> count to equal: "1" - expected "equal '1'" but got: "0"
+  return client.page.phoenixPage().expect.elements('@mainEl').count.to.equal(1)
 })
